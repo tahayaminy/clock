076b2cb3b272ddee0107b481cc9311121aac46dd
Anonymous <ParsWebDesigners@gmail.com>
2021-12-13 18:33:16 +0330

    fix logic

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const sec = document.getElementById("sec");
 const body = document.getElementById("body");
 const clock = document.getElementById("clock");
 
-setInterval(() => {
+function clockSet(){
   var now = new Date();
   var secT = (now.getSeconds() * 360) / 60;
   var minT = (now.getMinutes() * 360) / 60;
@@ -12,7 +12,7 @@ setInterval(() => {
   hour.style.transform = `rotate(${hourT + 90}deg)`;
   min.style.transform = `rotate(${minT + 90}deg)`;
   sec.style.transform = `rotate(${secT + 90}deg)`;
-  if (now.getHours() >= 17 || now.getHours() >= 6) {
+  if (now.getHours() <= 17 && now.getHours() >= 6) {
     body.style.backgroundColor = "#a2d2ff";
     clock.style.border = "5px solid #a2d2ff";
     clock.style.boxShadow = `5px 5px 10px #7193b3,
@@ -24,4 +24,12 @@ setInterval(() => {
     clock.style.boxShadow = `5px 5px 10px #20354c, -5px -5px 10px #3c638e,
     inset 5px 5px 10px #20354c, inset -5px -5px 10px #3c638e`;
   }
-}, 1000);
+}
+clockSet();
+
+
+setInterval(clockSet, 1000);
+setTimeout(()=>{
+  body.style.transition='60s';
+clock.style.transition='60s';
+},10000);
